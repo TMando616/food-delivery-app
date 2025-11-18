ed18a705fe34bc98d4cc75596f615b1d7ce2afab
TMando616 <t.mandokoro616@gmail.com>
2025-11-18 22:40:59 +0900

    feat: 基本セクションの作成 表示トグルボタンの作成

---
+++ src/components/section.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react"
+import TextToggleButton from "./text-toggle-button";
+
+interface SectionProps {
+    children: ReactNode;
+    title: string;
+}
+
+export default function Section({children, title}: SectionProps) {
+    return (
+        <section>
+            <div className="flex items-center justify-between py-4">
+                <h2 className="text-2xl font-bold">{title}</h2>
+                <TextToggleButton />
+            </div>
+            {children}
+        </section>
+    )
+}
