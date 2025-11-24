d39a3ceb625ca1e9bee3635cba1de31a66e5d9b7
TMando616 <t.mandokoro616@gmail.com>
2025-11-24 14:14:17 +0900

    feat: カテゴリ検索機能で取得した情報を表示

--- src/app/(private)/search/page.tsx
+++ src/app/(private)/search/page.tsx
@@ -1,5 +1,6 @@
+import Categories from '@/components/categories';
+import RestaurantList from '@/components/restaurant-list';
 import { fetchCategoryRetaurants } from '@/lib/restaurants/api';
-import React from 'react'
 
 export default async function SearchPage({searchParams}: {searchParams: Promise<{category: string}>}) {
 
@@ -9,11 +10,22 @@ export default async function SearchPage({searchParams}: {searchParams: Promise<
     if(category) {
         const {data:categoryRestaurants, error:fetchError} = await fetchCategoryRetaurants(category);
         console.log(categoryRestaurants)
+
+        return (
+            <>
+                <div className='mb-4'>
+                    <Categories />
+                </div>
+                {!categoryRestaurants ? (
+                    <p className='text-destructive'>{fetchError}</p>
+                ) : categoryRestaurants.length > 0 ? (
+                    <RestaurantList restaurants={categoryRestaurants}/>
+                ) : (
+                    <p>カテゴリ<strong>{category}</strong>に一致するレストランがありません。</p>
+                )}
+            </>
+        )
+
     }
 
-    return (
-        <div>
-            aaaaaaaaaaa
-        </div>
-    )
 }
