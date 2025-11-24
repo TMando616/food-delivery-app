da5c7787d4ecef60bbba80a9dd4a6c4df3e77436
TMando616 <t.mandokoro616@gmail.com>
2025-11-24 17:57:10 +0900

    feat: 検索バーのサジェスチョンの基本機能の実装

--- src/components/place-search-bar.tsx
+++ src/components/place-search-bar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Command,
     CommandEmpty,
@@ -5,19 +7,38 @@ import {
     CommandItem,
     CommandList,
 } from "@/components/ui/command"
+import { useState } from "react";
 
 export default function PlaceSearchBar() {
+
+    const [open, setOpen] = useState(false);
+    const [inputText, setInputText] = useState("")
+    const handleBlur = () => {
+        setOpen(false)
+    }
     return (
         <Command className="overflow-visible bg-muted" shouldFilter={false}>
-            <CommandInput placeholder="Type a command or search..."/>
-            <div className="relative">
-                <CommandList className="absolute bg-background w-full">
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    <CommandItem>Calendar</CommandItem>
-                    <CommandItem>Search Emoji</CommandItem>
-                    <CommandItem>Calculator</CommandItem>
-                </CommandList>
-            </div>
+            <CommandInput 
+                value={inputText}
+                placeholder="Type a command or search..."
+                onValueChange={(text) => {
+                    if(!open){
+                        setOpen(true)
+                    }
+                    setInputText(text)
+                }}
+                onBlur={handleBlur}
+            />
+            {open && (
+                <div className="relative">
+                    <CommandList className="absolute bg-background w-full">
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandItem>Calendar</CommandItem>
+                        <CommandItem>Search Emoji</CommandItem>
+                        <CommandItem>Calculator</CommandItem>
+                    </CommandList>
+                </div>
+            )}
         </Command>
     )
 }
