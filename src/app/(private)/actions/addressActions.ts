29f8c76456c8c7ef4f0de4731907ebf2e866fa11
TMando616 <t.mandokoro616@gmail.com>
2025-11-30 14:14:07 +0900

    PlaceDetailsAPIのエラーハンドイング

--- src/app/(private)/actions/addressActions.ts
+++ src/app/(private)/actions/addressActions.ts
@@ -9,4 +9,8 @@ export async function selectSuggestionAction(
 ) {
     const {data:locationData , error} = await getPlaceDetails(suggestion.placeId, ["location"], sessionToken)
     console.log(locationData)
+
+    if(error || !locationData || !locationData.location || !locationData.location.latitude || !locationData.location.longitude) {
+        throw new Error("住所情報を取得できませんでした")
+    }
 }
