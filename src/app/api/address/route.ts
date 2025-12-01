c13beeb9e85601c175e59471f3258c5c59a2992c
TMando616 <t.mandokoro616@gmail.com>
2025-12-01 23:06:50 +0900

    feat: 住所取得のルートハンドラーの作成

---
+++ src/app/api/address/route.ts
@@ -0,0 +1,36 @@
+import { Address } from "@/types"
+import { createClient } from "@/utils/supabase/server"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+    try {
+        // 選択中の住所情報をテーブルから取得
+
+        let addressList: Address[] = []
+        let selectedAddress: Address | null= null
+
+
+        const supabase = await createClient()
+
+        const {data: { user }, error: userError} = await supabase.auth.getUser()
+        if(userError || !user) {
+            return NextResponse.json({error: "ユーザーが認証されていません"}, {status: 401})
+        }
+        // 住所情報をテーブルから取得
+        const {data: addressData, error:addressError} = await supabase
+                .from("addresses")
+                .select("id,name,address_text,latitude,longitude")
+                .eq("user_id", user.id)
+
+        if(addressError) {
+            return NextResponse.json({error: "住所情報の取得失敗"}, {status: 500})
+        }
+
+        addressList = addressData
+
+        return NextResponse.json({addressList, selectedAddress})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error: "例外的なエラーが発生しました"})
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -72,3 +72,11 @@ export interface PlaceDetailsAll {
         longitude?: number,
     }
 }
+
+export interface Address {
+    id: number,
+    name: string,
+    address_text: string,
+    latitude: number,
+    longitude: number,
+}
