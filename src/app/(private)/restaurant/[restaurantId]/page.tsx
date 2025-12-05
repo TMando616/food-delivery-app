fe7542019a1f017b50983608e5c9d9b8f60baaaa
TMando616 <t.mandokoro616@gmail.com>
2025-12-05 23:33:31 +0900

    feat: 店舗情報画面の実装

--- src/app/(private)/restaurant/[restaurantId]/page.tsx
+++ src/app/(private)/restaurant/[restaurantId]/page.tsx
@@ -1,9 +1,44 @@
+import { Button } from '@/components/ui/button'
+import { getPlaceDetails } from '@/lib/restaurants/api'
+import { Heart } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
-export default function RestaurantPage() {
+export default async function RestaurantPage({params, searchParams} : {
+  params: Promise<{restaurantId: string }>
+  searchParams: Promise<{sessionToken: string }>
+}) {
+  const { restaurantId } = await params
+  const { sessionToken } = await searchParams
+  console.log(restaurantId, sessionToken)
+  // const {} = await getPlaceDetails()
   return (
     <div>
-      RestaurantPage
+      <div className="h-64 rounded-xl shadow-md relative overflow-hidden">
+        <Image 
+          src={"/no_image.png"}
+          fill
+          alt={"レストラン画像"}
+          className="object-cover"
+          priority
+          sizes={"(max-width: 1200px) 100vw, 1200px"}
+        />
+        <Button
+          size="icon"
+          variant="outline"
+          className='absolute top-4 right-4 shadow rounded-full'
+        >
+          <Heart color='gray' strokeWidth={3} size={15} />
+        </Button>
+      </div>
+      <div className='mt-4 flex items-center justify-between'>
+        <div>
+          <h1 className='text-3l font-bold'>レストラン名</h1>
+        </div>
+        <div className='flex-1'>
+          <div className='ml-auto w-80 bg-yellow-200'>検索バー</div>
+        </div>
+      </div>
     </div>
   )
 }
