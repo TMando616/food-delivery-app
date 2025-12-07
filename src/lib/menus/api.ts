34d6c42940439e9fceee5bcf9f1c8b373b02bc1e
TMando616 <t.mandokoro616@gmail.com>
2025-12-07 12:28:49 +0900

    feat: メニュー情報取得APIの実装

--- src/lib/menus/api.ts
+++ src/lib/menus/api.ts
@@ -1,4 +1,21 @@
+import { createClient } from "@/utils/supabase/server"
+
 export async function fetchCategoryMenus(primaryType: string) {
-    console.log(primaryType)
+
+    const supabase = await createClient()
+    
+    const { data: menus, error:menusError } = await supabase
+        .from('menus')
+        .select('*')
+        .eq("genre", primaryType)
+
+    if(menusError) {
+        console.error("メニューの取得に失敗しました。", menusError)
+        return {error: "メニューの取得に失敗しました。"}
+    }
+
+    if(menus.length === 0) {
+        return {data: []}
+    }
 
 }
