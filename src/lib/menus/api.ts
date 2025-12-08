8ecb84f25fd2916874e397aafb8bb0fe39a55017
TMando616 <t.mandokoro616@gmail.com>
2025-12-08 21:40:23 +0900

    feat: 注目商品データの整形

--- src/lib/menus/api.ts
+++ src/lib/menus/api.ts
@@ -18,4 +18,22 @@ export async function fetchCategoryMenus(primaryType: string) {
         return {data: []}
     }
 
+    const categoryMenus = []
+
+    const featuredItems = menus
+        .filter((menu) => menu.is_featured)
+        .map((menu) => (
+            {
+                id: menu.id,
+                photoUrl: supabase.storage.from("menus").getPublicUrl(menu.image_path).data.publicUrl,
+                name: menu.name,
+                price: menu.price
+            }
+        ))
+    
+    categoryMenus.push({
+        categoryName: "注目商品",
+        id: "featured",
+        items: featuredItems
+    })
 }
