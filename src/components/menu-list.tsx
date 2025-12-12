38e0b1e71fd45efd156f68c41605e062547bb680
TMando616 <t.mandokoro616@gmail.com>
2025-12-12 16:41:37 +0900

    feat: HP上のメニュー全商品表示機能実装

--- src/components/menu-list.tsx
+++ src/components/menu-list.tsx
@@ -1,15 +1,18 @@
 import { Menu } from '@/types'
+import Image from 'next/image'
 import React from 'react'
+import MenuCard from './menu-card'
 
 interface MenuListProps {
     menus: Menu[]
 }
 
 export default function MenuList({menus} :MenuListProps) {
-  return (
-    <div>
-      MenuList
-
-    </div>
-  )
+    return (
+        <ul className="grid grid-cols-6 gap-4">
+            {menus.map((menu) => (
+                <MenuCard key={menu.id} menu={menu} />
+            ))}
+        </ul>
+    )
 }
