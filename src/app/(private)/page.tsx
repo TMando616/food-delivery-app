7b32beb1dfc9686e0eef88ae2f9bf377b4406ff1
TMando616 <t.mandokoro616@gmail.com>
2025-12-12 16:36:08 +0900

    feat: HPメニューリストの表示実装

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,5 +1,7 @@
 import CarouselContainer from "@/components/carousel-container";
 import Categories from "@/components/categories";
+import MenuCard from "@/components/menu-card";
+import MenuList from "@/components/menu-list";
 import RestaurantCard from "@/components/restaurant-card";
 import RestaurantList from "@/components/restaurant-list";
 import Section from "@/components/section";
@@ -11,7 +13,8 @@ export default async function Home() {
   const { data:nearbyRamenRestaurants , error: nearbyRamenRestaurantError } = await fetchRamenRestaurants(lat, lng);
   const { data:nearbyRestaurants , error: nearbyRestaurantError } = await fetchRestaurants(lat, lng);
 
-  const primaryType = nearbyRamenRestaurants?.[0]?.primaryType
+  const restaurant = nearbyRamenRestaurants?.[0]
+  const primaryType = restaurant?.primaryType
   const { data:menus , error: menusError } = primaryType ? await fetchMenus(primaryType): { data: []};
 
   return (
@@ -41,6 +44,21 @@ export default async function Home() {
           <CarouselContainer slideToShow={4}>
             {nearbyRamenRestaurants.map((restaurant, index) => (
               <RestaurantCard key={index} restaurant={restaurant} />
+            ))}
+          </CarouselContainer>
+        </Section>
+      ) : (
+        <p>近くにラーメン店がありません。</p>
+      )}
+
+    {/* メニュー情報 */}
+      {!menus ? (
+        <p>{menusError}</p>
+      ) : menus.length > 0 ? (
+        <Section title={restaurant?.restaurantName}  expandedContent={<MenuList menus={menus}/>}>
+          <CarouselContainer slideToShow={6}>
+            {menus.map((menu) => (
+              <MenuCard key={menu.id} menu={menu} />
             ))}
           </CarouselContainer>
         </Section>
---
+++ src/components/menu-list.tsx
@@ -0,0 +1,15 @@
+import { Menu } from '@/types'
+import React from 'react'
+
+interface MenuListProps {
+    menus: Menu[]
+}
+
+export default function MenuList({menus} :MenuListProps) {
+  return (
+    <div>
+      MenuList
+
+    </div>
+  )
+}
