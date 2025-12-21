b6feb46ae789f56b8575a45379ddef2760fd7681
TMando616 <t.mandokoro616@gmail.com>
2025-12-21 11:01:05 +0900

    feat: sumItemsの実装

--- src/lib/cart/utils.ts
+++ src/lib/cart/utils.ts
@@ -9,8 +9,15 @@ export function computeCartDisplayLogic(carts: Cart[] | undefined) {
     // カート1件だけ
     if(carts.length === 1) {
         const only = carts[0]
-        return {displayMode: "cartSheet", sheetCart: only, cartCount: sumItems(only)}
+        return {
+            displayMode: "cartSheet", 
+            sheetCart: only, 
+            cartCount: sumItems(only)
+        }
     }
 
     return {displayMode: "cartDropDown", sheetCart: null, cartCount: 0}
 }
+
+const sumItems = (cart: Cart) =>
+    cart.cart_items.reduce((sum, item) => sum + item.quantity, 0)
