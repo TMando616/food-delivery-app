ef9c146106295a7f8c39c19d9bdf72a950f2c393
TMando616 <t.mandokoro616@gmail.com>
2025-12-28 16:57:40 +0900

    feat: 複数のカードが存在する場合の処理

--- src/components/cart-drop-down.tsx
+++ src/components/cart-drop-down.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from './ui/dropdown-menu'
+import { ShoppingCart } from 'lucide-react'
+import Image from 'next/image'
+import { Cart } from '@/types'
 
-export default function CartDropDown() {
+interface CartDropDownProps {
+  carts: Cart[],
+}
+
+export default function CartDropDown({carts}: CartDropDownProps) {
   return (
     <div>
-      CartDropDown
+      <DropdownMenu>
+        <DropdownMenuTrigger className='relative cursor-pointer'>
+          <ShoppingCart />
+          <span className='absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-green-700 rounded-full size-4 text-xs text-primary-foreground flex items-center justify-center'>
+            {10}
+          </span>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent className='w-[420px]'>
+          <DropdownMenuItem className='flex items-center p-4 justify-between'>
+            <div className='flex gap-3 flex-1 min-w-0'>
+              <div className='w-[64px] h-[64px] relative overflow-hidden rounded-full flex-none'>
+                <Image 
+                  fill
+                  src={"/no_image.png"}
+                  alt={'レストラン名'}
+                  className='object-cover w-full h-full'
+                />
+              </div>
+              <div className="felx-1 min-w-0">
+                <p className="font-bold text-base">{"レストラン名"}</p>
+                <p>小計： ￥{5000}</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-center size-7 font-medium rounded-full bg-primary text-popover text-xs">
+              {5}
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   )
 }
--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -6,7 +6,7 @@ import CartSheet from "./cart-sheet"
 import CartDropDown from "./cart-drop-down"
 
 export default function Cart() {
-    const { carts } = useCart()
+    const { carts, isLoading, cartsError } = useCart()
     console.log(carts)
 
     const { 
@@ -15,9 +15,16 @@ export default function Cart() {
         cartCount 
     } = computeCartDisplayLogic(carts)
 
+    if(cartsError) {
+        return <div>{cartsError.message}</div>
+    }
+    if(isLoading || !carts) {
+        return <div>loading</div>
+    }
+
     return displayMode === "cartSheet" ? (
         <CartSheet cart={sheetCart} count={cartCount}/>
     ) : (
-        <CartDropDown />
+        <CartDropDown carts={carts}/>
     )
 }
