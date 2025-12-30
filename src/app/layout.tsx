a4cafc0477e98bd6361328d99de97dec6295f181
TMando616 <t.mandokoro616@gmail.com>
2025-12-30 12:20:35 +0900

    feat: カートの開閉状態をグローバルに管理

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ModalProvider } from "./context/modalContext";
+import { CartProvider } from "./context/cartContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ModalProvider>
-          {children}
+          <CartProvider>
+            {children}
+          </CartProvider>
         </ModalProvider>
       </body>
     </html>
