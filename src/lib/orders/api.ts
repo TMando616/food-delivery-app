662d7eee558d4ae5e90bade9b58d8a8d09324b3d
TMando616 <t.mandokoro616@gmail.com>
2026-01-15 07:22:59 +0900

    feat: 注文履歴情報を取得

--- src/lib/orders/api.ts
+++ src/lib/orders/api.ts
@@ -1,3 +1,42 @@
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+
 export async function fetchOrders () {
+
+    const supabase = await createClient()
+
+    const { 
+        data: { user }, 
+        error: userError
+    } = await supabase.auth.getUser()
     
+    if(userError || !user) {
+        redirect('/login')
+    }
+
+    const { data: orders, error: ordersError } = await supabase
+        .from('orders')
+        .select(`
+            id,
+            restaurant_id,
+            created_at,
+            fee,
+            service,
+            delivery,
+            subtotal_price,
+            total_price,
+            order_items (
+                id,
+                price,
+                quanaity,
+                name,
+                image_path,
+            )
+        `)
+        .eq('user_id', user.id)
+
+    if(ordersError) {
+        console.error('注文履歴の取得に失敗しました。', ordersError)
+        throw new Error('注文履歴の取得に失敗しました。')
+    }
 }
