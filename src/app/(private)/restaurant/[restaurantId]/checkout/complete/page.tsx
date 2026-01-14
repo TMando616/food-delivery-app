8dacaabcbe356c730f6f44e86b06376a40fac288
TMando616 <t.mandokoro616@gmail.com>
2026-01-14 17:04:52 +0900

    feat: アニメーションの実装

--- src/app/(private)/restaurant/[restaurantId]/checkout/complete/page.tsx
+++ src/app/(private)/restaurant/[restaurantId]/checkout/complete/page.tsx
@@ -1,11 +1,13 @@
+import CookingAnimation from "@/components/cooking-animation";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
 
 export default function CheckoutCompletePage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] gap-4">
             <h1 className="font-bold text-3xl">ご注文の品を準備しています…</h1>
-            <div>あにめ</div>
+            <CookingAnimation />
                 <Button size={"lg"} asChild>
                     <Link
                         href={"/orders"}
---
+++ src/components/cooking-animation.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function CookingAnimation() {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/998ee982-9fed-4992-8f71-c6eaf9f29f4b/LPugbOkiZS.lottie"
+      loop
+      autoplay
+      className='w-2/3'
+    />
+  )
+}
